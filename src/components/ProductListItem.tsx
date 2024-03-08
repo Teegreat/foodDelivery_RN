d5f20e57a1498d9809b51fba273fb496354f2b31
TeeGreat <toyinatofarati64@gmail.com>
2024-03-08 16:05:27 +0100

    dynamic routes

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
 import Colors from '@/constants/Colors';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, StyleSheet, Text } from 'react-native';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
       
 export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
@@ -11,11 +12,13 @@ type ProductListItemProps = {
 
 const ProductListItem = ({product}: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
+    <Link href={`/${product.id}`} asChild>
+    <Pressable style={styles.container}>
       <Image source={{uri: product.image || defaultPizzaImage }} style={styles.image} resizeMode='contain' />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
-    </View>
+    </Pressable>
+    </Link>
   );
 }
 
