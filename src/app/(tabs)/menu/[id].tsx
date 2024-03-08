5b100abc6c126b55753360b5a9969b9d7e85317f
TeeGreat <toyinatofarati64@gmail.com>
2024-03-08 16:32:34 +0100

    expo routers and screens

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import { Stack } from 'expo-router'
 
 const ProductDetailsScreen = () => {
   const {id} = useLocalSearchParams()
   return (
     <View>
+      <Stack.Screen options={{title: 'Details: ' + id}} />
+
       <Text>ProductDetailsScreen for id: {id}</Text>
     </View>
   )
