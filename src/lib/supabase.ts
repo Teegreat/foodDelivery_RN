691df2de4369b4890a8dbbd14db8a58265d2c70c
TeeGreat <toyinatofarati64@gmail.com>
2024-03-11 05:49:09 +0100

    supabase config

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://nypszntzbdwgclsfkmwn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55cHN6bnR6YmR3Z2Nsc2ZrbXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxMzIxNTQsImV4cCI6MjAyNTcwODE1NH0.1nw5UaDm40JEIxYv9mCqezWNpwlGBTsPhehfaXScW1w';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
