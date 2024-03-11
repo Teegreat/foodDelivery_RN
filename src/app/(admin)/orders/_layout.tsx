2babbc4dffcd545a367e1ca7d207a6e9d6dab2b4
TeeGreat <toyinatofarati64@gmail.com>
2024-03-11 05:33:14 +0100

    orders screen: Top Tab Navigation

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -4,9 +4,13 @@ import { Stack } from "expo-router";
 export default function MenuStack () {
     return (
         <Stack>
-            <Stack.Screen 
+            {/* <Stack.Screen 
                 name="index"
                 options={{title: 'Orders'}}
+            /> */}
+             <Stack.Screen 
+                name="list"
+                options={{headerShown: false}}
             />
         </Stack>
     )
