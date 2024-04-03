4e43872334f5aa0011299e141669be20ca4bc489
TeeGreat <toyinatofarati64@gmail.com>
2024-04-03 01:52:54 +0100

    link customers info to stripe

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -3,22 +3,34 @@
 // This enables autocomplete, go to definition, etc.
 
 import { stripe } from "../_utils/stripe.ts"
+import { createOrRetrieveProfile } from "../_utils/supabase.ts"
 
 console.log("Hello from Functions!")
 
 
-Deno.serve(async (req) => {
+Deno.serve(async (req: Request) => {
   try {
     const { amount } = await req.json()
 
+    const customer = await createOrRetrieveProfile(req)
+    // Create an ephermeralKey so that the Stripe SDK can fetch the customer's stored payment methods.
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer },
+      { apiVersion: "2020-08-27" }
+    );
+
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
-      currency: 'usd'
+      currency: 'usd',
+      customer: customer,
+      // ephemeralKey: ephemeralKey.secret
     })
 
     const res = {
       paymentIntent: paymentIntent.client_secret,
-      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY')
+      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer,
     }    
 
   return new Response(
